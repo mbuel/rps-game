ca6c80d2d0eb7306225ca3bf1c2c1c6b261904f8
mbuel <morris.buel@gmail.com>
2021-03-22 00:16:52 -0700

    game images now changing with 'play' button

--- js/game.js
+++ js/game.js
@@ -1,13 +1,18 @@
 class RockPaperScissors {
     numberOfRounds = 1;
+    maxChoices = 3;
     playerScore = 0;
+    playerRoll = 0;
+    computerRoll =0;
     computerScore = 0;
+    currentRound = 0;
     playerImage = document.getElementById('player-image');
     computerImage = document.getElementById('computer-image');
     newGame = document.getElementById('btn-new').addEventListener('click', this.init.bind(this));
     startGame = document.getElementById('btn-start').addEventListener('click', this.start.bind(this));
     computerScoreDom = document.getElementById('score-computer-current');
     playerScoreDom = document.getElementById('score-player-current');
+    roundDisplayDom = document.getElementById('current-round');
     roundSelector = document.getElementById('rounds-choice');
     playerChoice = document.getElementById('rps-choice');
     
@@ -17,18 +22,48 @@ class RockPaperScissors {
 
     init() {
         console.log(this.roundSelector.selectedOptions[0].value);
-        console.log(this.playerChoice.selectedOptions[0].value);
+        console.log(this.playerChoice);
 
         this.playerImage.classList = 'cardProfile player-default';
         this.computerImage.classList = 'cardProfile computer-default';
         this.playerScore = 0;
+        this.playerRoll = 0;
+        this.computerRoll = 0;
         this.computerScore = 0;
+        this.currentRound = 0;
         this.playerScoreDom.textContent = this.playerScore;
         this.computerScoreDom.textContent = this.computerScore;
+        this.roundDisplayDom.textContent = this.currentRound;
+    }
+
+    castingLots(max) {
+        let currentRoll = Math.ceil(Math.random() * max);
+        return currentRoll;
+    }
+
+    setImageText(roll) {
+        switch(roll) {
+            case 1:
+                return 'rock';
+            case 2:
+                return 'scissors';
+            case 3:
+                return 'paper';
+        }
     }
 
     start() {
-        console.log('start game');
+        // this.playerRoll = this.castingLots(this.maxChoices);
+        this.playerRoll = this.playerChoice.selectedIndex + 1;
+        this.computerRoll = this.castingLots(this.maxChoices);
+
+        this.playerImage.classList = 'cardProfile ' + this.setImageText(this.playerRoll);
+        this.computerImage.classList = 'cardProfile ' + this.setImageText(this.computerRoll);
+
+        // Determine winner and set score
+
+        // check if last round
+        // set game winner if last round
     }
 }
 
